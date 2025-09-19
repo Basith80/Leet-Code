e21a664fd0073304b3ecb208841741a1f0c87f5f
Abdul Basith <abdulbasith.808080@gmail.com>
2025-09-19 06:52:11 +0530

    Time: 0 ms (100%), Space: 42.6 MB (60.05%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,7 +10,26 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummyNode = new ListNode();
+        ListNode dummy = new ListNode();
+        ListNode node = dummy;
+
+     while (l1 != null && l2 != null) {
+            if(l1.val <= l2.val){
+                node.next = l1;
+                l1 = l1.next;
+            }else{
+                node.next = l2;
+                l2 = l2.next ;
+            }
+            node = node.next;
+
+        }
+        node.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+}
+/*
+ ListNode dummyNode = new ListNode();
         ListNode curNode = dummyNode;
         while (l1 != null && l2 != null) {
             if (l1.val <= l2.val) {
@@ -25,5 +44,4 @@ class Solution {
         }
         curNode.next = l1 != null ? l1 : l2;
         return dummyNode.next;
-    }
-}
+*/
