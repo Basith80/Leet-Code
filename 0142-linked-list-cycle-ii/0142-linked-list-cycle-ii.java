e77c031efdab892b3a2d62a44a0a26fe91553a9d
Abdul Basith <abdulbasith.808080@gmail.com>
2025-09-19 09:58:57 +0530

    Time: 0 ms (100%), Space: 44.5 MB (84.48%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+        ListNode start = head;
+        ListNode inter = findIntersect(head);
+        if (inter == null)
+            return null;
+        while (start != inter) {
+            start = start.next;
+            inter = inter.next;
+        }
+
+        return inter;
+    }
+
+    private ListNode findIntersect(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return fast;
+            }
+        }
+        return null;
+    }
+}
