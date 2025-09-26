fd13b80d58eb647036014726620b6b3f3f24cc7c
Abdul Basith <abdulbasith.808080@gmail.com>
2025-09-26 07:18:24 +0530

    Time: 1 ms (99.62%), Space: 41.1 MB (99.94%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,43 @@
 class Solution {
     public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack[++top]=ch;
+            }
+            else{
+                if(top==-1) return false;
+                char open = stack[top--];
+                if(ch == ')' && open != '(') return false;
+                if(ch == ']' && open != '[') return false;
+                if(ch == '}' && open != '{') return false;
+            }
+        }
+        return top==-1;
+    }
+}
+
+/**
+char[] stack = new char[s.length()];
+        int top = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack[++top]=ch;
+            }
+            else{
+                if(top==-1) return false;
+                char open = stack[top--];
+                if(ch == ')' && open != '(') return false;
+                if(ch == ']' && open != '[') return false;
+                if(ch == '}' && open != '{') return false;
+            }
+        }
+        return top==-1; */
+
+        /*
         Stack<Character> stack = new Stack<>();
         for(char ch : s.toCharArray()){
             if(ch == '(' || ch == '{' || ch == '['){
@@ -24,23 +62,4 @@ class Solution {
             }
         }
         return stack.isEmpty();
-    }
-}
-
-/**
-char[] stack = new char[s.length()];
-        int top = -1;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if(ch == '(' || ch == '[' || ch == '{'){
-                stack[++top]=ch;
-            }
-            else{
-                if(top==-1) return false;
-                char open = stack[top--];
-                if(ch == ')' && open != '(') return false;
-                if(ch == ']' && open != '[') return false;
-                if(ch == '}' && open != '{') return false;
-            }
-        }
-        return top==-1; */
+        */
