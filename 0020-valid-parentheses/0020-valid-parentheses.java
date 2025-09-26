358d784d9a5faf34b25ae56710446dc3a9e143a9
Abdul Basith <abdulbasith.808080@gmail.com>
2025-09-26 07:17:39 +0530

    Time: 2 ms (96.84%), Space: 41.5 MB (95.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,17 +1,46 @@
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(')');
-            } else if (c == '{') {
-                stack.push('}');
-            } else if (c == '[') {
-                stack.push(']');
-            } else if (stack.isEmpty() || stack.pop() != c) {
-                return false;
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            if(ch == ')'){
+                if(stack.isEmpty() || stack.pop() != '('){
+                    return false;
+                }
+            }
+
+            if(ch == '}'){
+                if(stack.isEmpty() || stack.pop() != '{'){
+                    return false;
+                }
+            }
+
+            if(ch == ']'){
+                if(stack.isEmpty() || stack.pop() != '['){
+                    return false;
+                }
             }
         }
         return stack.isEmpty();
     }
 }
+
+/**
+char[] stack = new char[s.length()];
+        int top = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack[++top]=ch;
+            }
+            else{
+                if(top==-1) return false;
+                char open = stack[top--];
+                if(ch == ')' && open != '(') return false;
+                if(ch == ']' && open != '[') return false;
+                if(ch == '}' && open != '{') return false;
+            }
+        }
+        return top==-1; */
