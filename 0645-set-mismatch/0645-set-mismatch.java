6406a974ee252e334db7de7ee4fdaaffb4f6c194
Abdul Basith <abdulbasith.808080@gmail.com>
2025-10-15 07:43:36 +0530

    Time: 3 ms (78.23%), Space: 46.1 MB (19.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -14,11 +14,12 @@ class Solution {
             }
         }
 
-        for(int i=0 ; i< arr.length ; i++){
-            if(arr[i] != i+1){
-                return new int[]{arr[i] , i+1};
+        for(int j =0 ;j<arr.length ; j++){
+            if(j+1 != arr[j]){
+                return new int[] {arr[j] , j+1};
             }
         }
+
         return new int[]{-1,-1};
     }
 }
