0b572532f03bb14942c1df0949f56f64faca4e52
Abdul Basith <abdulbasith.808080@gmail.com>
2025-10-31 12:06:47 +0530

    Time: 383 ms (60.33%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,3 +1,3 @@
-select all p.firstName , p.lastName , a.city , a.state  from 
+select p.firstName , p.lastName , a.city , a.state  from 
 Person as p left join Address as a 
 on p.personId = a.personId;
