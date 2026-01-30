827281d902c643368eb5fe858f295c09cb6a379f
Abdul Basith <abdulbasith.808080@gmail.com>
2026-01-30 18:18:56 +0530

    Time: 16 ms (15.55%), Space: 47.1 MB (35.76%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,29 +1,29 @@
 class Solution {
     public String reverseWords(String s) {
         // Abdul Basith
-        String t = "";
-        String res = "";
+        StringBuilder t = new StringBuilder();
+        StringBuilder res = new StringBuilder();
 
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i) != ' '){
-                t+=s.charAt(i);
+                t.append(s.charAt(i));
             }
             if(s.charAt(i) == ' ' || i == s.length()-1){
-                res+= reverse(t);
-                t="";
+                res.append(reverse(t.toString()));
+                t.setLength(0);
                 if(i != s.length()-1){
-                    res += " ";
+                    res.append(" ");
                 }
             }
         }
-        return res;
+        return res.toString();
 
     }
     private static String reverse(String s){
-        String ans= "";
+         StringBuilder ans = new StringBuilder();
         for(int i=s.length()-1; i>=0; i--){
-            ans += s.charAt(i);
+            ans.append(s.charAt(i));
         }
-        return ans;
+        return ans.toString();
     }
 }
