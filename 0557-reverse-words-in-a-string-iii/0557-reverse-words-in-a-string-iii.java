22e6546d29fd1edc8e36b67e3156707b5b93e21d
Abdul Basith <abdulbasith.808080@gmail.com>
2026-02-11 20:01:09 +0530

    Time: 48 ms (5.42%), Space: 47.5 MB (9.37%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,29 +1,34 @@
 class Solution {
     public String reverseWords(String s) {
-        // Abdul Basith
-        StringBuilder t = new StringBuilder();
-        StringBuilder res = new StringBuilder();
+        return rotate(s);
+    }
+
+    private static String rotate(String s){
+        String ans = "";
+        StringBuilder sb = new StringBuilder();
 
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) != ' '){
-                t.append(s.charAt(i));
-            }
-            if(s.charAt(i) == ' ' || i == s.length()-1){
-                res.append(reverse(t.toString()));
-                t.setLength(0);
-                if(i != s.length()-1){
-                    res.append(" ");
+            if(s.charAt(i) == ' '){
+                ans+= reverse(sb);
+                ans+=" ";
+                sb = new StringBuilder();
+            }else{
+                sb.append(s.charAt(i));
+                if(i == s.length()-1){
+                    ans+= reverse(sb);
                 }
             }
         }
-        return res.toString();
+        return ans;
+    }
 
-    }
-    private static String reverse(String s){
-         StringBuilder ans = new StringBuilder();
-        for(int i=s.length()-1; i>=0; i--){
-            ans.append(s.charAt(i));
+    private static String reverse(StringBuilder sb){
+        int l = sb.length()-1;
+        String ans = "";
+        for(int i=l; i>=0; i--){
+            ans+=sb.charAt(i);
         }
-        return ans.toString();
+        return ans;
     }
+
 }
