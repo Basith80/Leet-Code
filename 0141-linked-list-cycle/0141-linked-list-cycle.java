de074fa17d18aa303b74b89aa23ae0fbb3453e2c
Abdul Basith <abdulbasith.808080@gmail.com>
2026-03-17 12:37:37 +0530

    Time: 0 ms (100%), Space: 46.8 MB (68.31%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,15 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null) return false;
         ListNode f = head;
         ListNode s = head;
-        while(f != null && f.next != null){
-            f = f.next.next;
-            s = s.next;
-            if(f == s){
-                return true;
-            }
+        while(s != null && s.next != null){
+            f = f.next;
+            s = s.next.next;
+            if(f == s)return true;
         }
-return false;
+        return false;
     }
 }
